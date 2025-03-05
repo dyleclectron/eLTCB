13ea8a4ce4c7145fa710fb7ffc5d48a3bbb12cad
dyl006 <duymanh1252006@gmail.com>
2025-03-05 10:28:43 +0700

    up

--- Ex7/house1.c
+++ Ex7/house1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-int fibonaci(int n)
-{
-    return n==1||n==2?1:fibonaci(n-1)+fibonaci(n-2);
-}
+// int fibonaci(int n)
+// {
+//     return n==1||n==2? 1 : fibonaci(n-1)+fibonaci(n-2);
+// }
 int main()
 {
     int n;scanf("%d",&n);
-    printf("%d",fibonaci(n));
+    // printf("%d",fibonaci(n));
+    int a[n+2];a[1]=a[2]=1;
+    for(int i=3;i<=n;i++) a[i]=a[i-1]+a[i-2]; 
+    printf("%d",a[n]);
     return 0;
 }
